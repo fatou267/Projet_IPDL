021886b9e3e1123106d5e417a36adee73d3c21f6
Jonathan Kuro <kurojojo08@gmail.com>
2021-07-23 01:11:06 +0000

    started to create the first page

---
+++ templates/home/home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/home.css" type="text/css">
+    <title>EDMI-Ecole Doctorale de Mathématiques et Informatique</title>
+</head>
+
+<body>
+    <div class="container">
+        <header class="header">
+            <div class="navbar">
+                <div class="logo">
+                    <a href="">
+                        <img src="/public/images/home/logo.gif" alt="">
+                    </a>
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">Accueil</a>
+                        </li>
+                        <li>
+                            <a href=" #">A propos</a>
+                        </li>
+                        <li>
+                            <a href="#">Admission</a>
+                        </li>
+                        <li>
+                            <a href=" #">Espace utilisateur</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="title">
+
+                <h1>ECOLE DOCTORALE MATHÉMATIQUES-INFORMATIQUE</h1>
+                <h2>Université Cheikh Anta Diop de Dakar</h2>
+            </div>
+        </header>
+        <section class="about">
+            <img src="/public/images/home/about.png" alt="">
+
+            <div class="content">
+                <h1>À PROPOS</h1>
+                <p>
+                    L’école doctorale de Mathématiques et Informatique de l’Université Cheikh Anta Diop de Dakar a pour but de coordonner et d’organiser toutes les études doctorales en Mathématiques et Informatique à l’UCAD, aussi bien la formation à la recherche que la
+                    production scientifique en Mathématique, en Informatique ainsi que dans les domaines qui leur sont connexes. Son siège est au département de Mathématiques et Informatique de la Faculté des Sciences et Techniques. L’école doctorale
+                    comporte deux mentions : Mathématiques- Modélisation et Informatique-Télécommunications. Nous vous invitons à parcourir notre site et à découvrir les formations et la communauté dont chaque élève de EDMI bénéficie. Contactez-nous dès
+                    aujourd'hui pour planifier une visite, en apprendre plus sur les modalités d'inscription ou poser toute autre question.
+                </p>
+                <button>En savoir plus</button>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>

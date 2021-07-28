f88a27fd35a8ba056d88b4de4bf998da8106e242
fatou267 <67634804+fatou267@users.noreply.github.com>
2021-07-28 02:09:39 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,283 @@
-<?php
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Ecole doctorale</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 
-use App\Controller\HomeController;
+	<link rel="stylesheet" type="text/css" media="screen" href="index.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="./image/index.gif"></a>
+<ul class="nav justify-content-end" style="margin-left: 40%;">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#group0" style="color:  #2A7C6F;">Accueil</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#group1" style="color:  #2A7C6F;">A propos</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#group2" style="color:  #2A7C6F;">Admission</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#group3" style="color:  #2A7C6F;">Espace Utilisateur</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#group4" tabindex="-1" style="color:  #2A7C6F;">Contact</a>
+  </li>
+</ul>
+</div>
+</nav>
+<section id="group0">
+  <div class="card bg-dark text-white">
+  <img src="./image/acc.jpeg" class="card-img" alt="...">
+  <div class="card-img-overlay text-center fst-italic">
+    <div class="position-absolute top-50 start-50 translate-middle">
+    <h1 class="card-title" style="color:  #2A7C6F;">ECOLE DOCTORALE </h1>
+    <h1 class="card-title" style="color:  #2A7C6F;">MATHÉMATIQUES-INFORMATIQUE</h1>
+    <p class="card-text" style="color:  #EEFAF7;">Université Cheikh Anta Diop de Dakar</p>
+    </div>
+  </div>
+</div>
+</section>
 
-require_once "vendor/autoload.php";
+<section id="group1" class="position">
+<div class="container">
+   <div class="row">
 
-$homeController = new HomeController();
+      <div class="col-md-3">
+          
+      </div>
+    <div class="col-md-6">
+  <div class="card mb-3" style="max-width: 540px; margin-top: 15%;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="./image/7.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body" style="color:  #2A7C6F;">
+        <h3 class="card-title">A propos</h3>
+      <p class="decoupe card-text mt-4 text-break">L’école doctorale de Mathématiques et Informatique de l’Université Cheikh Anta Diop de Dakar a pour but de coordonner et d’organiser toutes les études doctorales en Mathématiques et Informatique à l’UCAD, aussi bien la formation à la recherche que la production scientifique en Mathématique, en Informatique ainsi que dans les domaines qui leur sont connexes. Son siège est au département de Mathématiques et Informatique de la Faculté des Sciences et Techniques.
 
-if (isset($_SERVER['REQUEST_URI'])) {
-    if ($_SERVER['REQUEST_URI'] == '/') {
-        $homeController->home();
-    }
-}
+      L’école doctorale comporte deux mentions : Mathématiques- Modélisation et Informatique-Télécommunications.
+
+      Nous vous invitons à parcourir notre site et à découvrir les formations et la communauté dont chaque élève de EDMI bénéficie. Contactez-nous dès aujourd'hui pour planifier une visite, en apprendre plus sur les modalités d'inscription ou poser toute autre question.</p>
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
+ <div class="col-md-3">
+          
+  </div>
+  </div>
+  </div>  
+</section>
+
+
+
+<section id="group2">
+  <div >
+  <h3 class="text-center" style="color: #2A7C6F; margin-top: 5%;">E.D.M.I</h3>
+  </div>
+  <div class="row row-cols-1 row-cols-md-3 g-4" style="margin-top: 3%;">
+  <div class="col">
+    <div class="card h-100">
+      <img src="./image/2.jpeg" class="card-img-top" alt="...">
+
+      <div class="card-body titre text-center">
+        <div style="margin-top: 15%;  margin-bottom: 15%;">
+        <h5 class="card-title">PRÉSENTATION </h5>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="./image/1.jpeg" class="card-img-top" alt="...">
+      
+      <div class="card-body titre text-center">
+        <div style="margin-top: 15%;  margin-bottom: 15%;">
+        <h5 class="card-title">ADMISSIONS EXPLIQUÉES </h5>
+        
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="./image/rel.jpeg" class="card-img-top" alt="...">
+      <div class="card-body titre text-center">
+        <div style="margin-top: 15%;  margin-bottom: 15%;">
+        <h5 class="card-title ">NOS FILLIERES</h5>
+        </div>
+      </div>
+    
+    </div>
+  </div>
+</div>
+  
+  
+  
+</section>
+
+<section>
+  <div class="bg" >
+  <br><br><br><br><br><br><br><br><br>
+  <p class="text-center"><q>
+    Je crois au rationnel, pas au pouvoir magique de l’éducation.</q></p>
+  <p class="text-center fw-bolder">Maria Edgeworth</p>
+  <br><br><br><br><br><br><br><br><br>
+</div>
+</section>
+<section id="group3">
+  <h3 class="text-center" style="color: #2A7C6F; margin-top: 5%;">ADMISSIONS ET INSCRIPTION</h3>
+  <p class="orient">EDMI est fier(ère) d'ouvrir les admissions pour la prochaine année scolaire. Découvrez plus d'informations sur les modalités d'inscription, les prérequis, les frais et les aides financières ci-dessous. N'hésitez pas à nous contacter pour en savoir plus.</p>
+  <div class="card-group">
+  <div class="card">
+    <img src="./image/bu.jpeg" class="card-img-top" alt="...">
+    <div class="card-body titre text-center">
+      <h5 class="card-title" style="margin-top: 10%;">Ce qu’il faut savoir sur l’inscription</h5>
+      <p class="card-text" style="margin-top: 10%;">Nous vous invitons à entamer les démarches d'inscription à EDMI en prenant rendez-vous pour une visite de notre établissement. Une visite guidée personnalisée vous aidera à vous faire une idée de ce à quoi pourrait ressembler votre quotidien dans notre établissement. C'est également l'occasion pour nous de vous consacrer du temps, et de répondre à toutes vos questions.</p>
+      <div class="text-muted">
+      <button type="button" class="btn btn-success" style="margin-top: 15%;">Contact</button>
+    
+     </div></div>
+  </div>
+  <div class="card">
+    <img src="./image/ren.jpeg" class="card-img-top" alt="...">
+    <div class="card-body titre text-center">
+      <h5 class="card-title" style="margin-top: 10%;">N'attendez pas plus longtemps</h5>
+      <p class="card-text" style="margin-top: 10%;">Bien que les bulletins de note soient un facteur important dans l'admission à EDMI, nous prenons en considération de nombreux autres éléments. Réussite scolaire ou extra-scolaire, expériences et qualités personnelles; ces éléments, souvent sous-estimés sont pourtant les indicateurs précieux d'un accomplissement tant scolaire qu'individuel.</p>
+      <div class="text-muted">
+      <button type="button" class="btn btn-success" style="margin-top: 15%;">Contact</button>
+    
+    </div>
+    </div>
+  </div>
+  <div class="card">
+    <img src="./image/4.jpeg" class="card-img-top" alt="...">
+    <div class="card-body titre text-center">
+      <h5 class="card-title" style="margin-top: 10%;">Talents recherchés</h5>
+      <p class="card-text" style="margin-top: 10%;">Les notes ne représentent qu'une partie de la vie d'un étudiant, elle sont souvent trop prises en compte par les bureaux d'inscriptions des établissements. EDMI sélectionne ses élèves à partir d'éléments indiquant leur potentiel à venir. Ces caractéristiques incluent leur curiosité intellectuelle, leur persévérance face à l'adversité et leur créativité. Pour obtenir plus d'informations ou commencer votre inscription, veuillez contacter le bureau des admissions.</p>
+      <div class="text-muted">
+      <button type="button" class="btn btn-success" style="margin-top: 3%;">Contact</button>
+    
+    </div>
+
+    </div>
+  </div>
+</div>
+ 
+</section>
+<section id="group4">
+  <div class="card bg-dark text-white">
+  <img src="./image/acc.jpeg" class="card-img" alt="...">
+  <div class="card-img-overlay" style="margin-top:10%; margin-left:10%; margin-right: 10%;">
+   <div class="footer">
+  <div class="footer-content">
+    <div class="footer-section about">
+    <div class="card titre">
+
+  <div class="contact_header jumbotron text-center">
+  <h1 class="display-4">NOUS CONTACTER</h1>
+</div>
+  
+<div class="contact_form_wrapper container">
+  <div class="row" style="margin-left: 15%;">
+    <div class="col-sm-8">
+      <div class="contact_form">
+        <div class="row mt-2">
+          <div class="col-sm-12">
+            <div class="form-group">
+              <label>Nom*</label>
+              <br><br>
+              <input type="text" class="form-control" placeholder="Enter Name" >
+            </div>
+          </div>
+          
+        </div>
+        <div class="row mt-2">
+        <div class="col-sm-12">
+          <div class="form-group">
+              <label>Email*</label>
+              <br>
+              <input type="email" class="form-control" placeholder="Enter Email">
+            </div>
+          </div>
+        </div>
+        <div class="row mt-2">
+          <div class="col-sm-12">
+            <div class="form-group">
+              <label>Objet*</label>
+              <br>
+              <input type="text" class="form-control" placeholder="Enter Subject">
+            </div>
+          </div>
+        </div>
+        <div class="row mt-2">
+          <div class="col-sm-12">
+            <div class="form-group">
+              <label>Message*</label>
+              <br>
+              <textarea class="form-control">
+              </textarea>
+            </div>
+          </div>
+        </div>
+        <div class="row text-center mt-2">
+          <div class="col-sm-12">
+            <button class="btn btn-md btn-primary">Submit</button>
+            <br><br>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+  </div>
+ </div>
+</div>
+</div>
+</section>
+
+
+
+<br><br>
+
+<footer class="card-footer text-center">
+  <div class="footer-bottom">
+<small class="text-muted ">&copy;Copyright 2021 par EDMI</small>
+<br><br> 
+  <span class="icon-stack">
+  <i class="icon-check-empty icon-stack-base"></i>
+  <i class="icon-linkedin  icon-2x"></i>
+</span>
+<span class="icon-stack">
+  <i class="icon-check-empty icon-stack-base"></i>
+  <i class="icon-facebook  icon-2x"></i>
+</span>
+  <span class="icon-stack">
+  <i class="icon-check-empty icon-stack-base"></i>
+  <i class="icon-twitter  icon-2x"></i>
+</span>
+ </div>
+</footer>
+
+<br><br>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</body>
+</html>
+ 
